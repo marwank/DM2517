75bc3115228721a043804f9d9ed5837db4bf1f65
Marwan Khalili <marwank@kth.se>
2017-01-08 16:40:55 +0100

    fixed register

--- register.php
+++ register.php
@@ -1,21 +1,37 @@
 <?php
-$server = "localhost";
+$servername = "localhost";
 $username = "marwank";
 $password = "marwank-xmlpub16";
-$database = "marwank";
-$mysqli = new mysqli($server, $username, $password, $database);
+$dbname = "marwank";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 /* check connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
+/*
+$sql = "SHOW TABLES";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+*/
 $uname = $_POST['username'];
 $pword = $_POST['password'];
-$query = "INSERT INTO users (username, password) VALUES ($uname, $pword)";
-/* Create table doesn't return a resultset */
-if ($mysqli->query($query) === TRUE) {
-    printf("User successfully created.\n");
+
+$sql = "INSERT INTO Users (username, password) VALUES ('$uname', '$pword')";
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
+
+mysqli_close($conn);
 ?>
