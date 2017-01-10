331bc14ea7debed568c0c49aa8b6754ac2099b9e
Marwan Khalili <marwank@kth.se>
2017-01-10 14:05:38 +0100

    added user.php

---
+++ user.php
@@ -0,0 +1,23 @@
+<?php
+include('config.php');
+session_start();
+if (!isset($_SESSION['username'])) {
+  header("location:login.php");
+}
+$username = $_GET['username'];
+?>
+<html>
+<head>
+  <title>Welcome</title>
+</head>
+<body>
+  <?php
+  $sql = "SELECT image from Posts WHERE uid IN
+  (SELECT uid FROM Users WHERE username = '$username')";
+  $result = mysqli_query($conn, $sql);
+  while ($row = mysqli_fetch_assoc($result)) {
+    echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
+  }
+  ?>
+</body>
+</html>
