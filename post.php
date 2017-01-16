def06f8805f2ebbddce67471341896d1b8d421ba
Marwan Khalili <marwank@kth.se>
2017-01-16 15:32:52 +0100

    fixing post

--- post.php
+++ post.php
@@ -7,6 +7,9 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result)
 }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+}
 ?>
 <html>
 <body>
@@ -15,15 +18,13 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
     }
 
-
     if (isset($_SESSION['uid']) && ($_SESSION['uid'] == $row['uid'])) {
-        echo '<form action="post.php" method="post" enctype="text/plain">
+        echo '<form action="post.php?id=' echo '$id' '"method="post" enctype="text/plain">
                 Add a tag to your image:
                 <input type="text" accept="text/plain" name="usertag" id="usertag">
                 <input type="submit" value="Add tag" name="submit">
               </form>'
     }
-
     ?>
 </body>
 </html>
