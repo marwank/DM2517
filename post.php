72501bc88572a746245c3ac07eb938d76eb56479
Marwan Khalili <marwank@kth.se>
2017-01-16 15:40:52 +0100

    Fixed logout

--- post.php
+++ post.php
@@ -5,10 +5,14 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $id = $_GET['id'];
     $sql = "SELECT * FROM Posts WHERE id = '$id'";
     $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result)
-}
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+    $row = mysqli_fetch_assoc($result);
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $tag = $_POST['usertag'];
+      $sql = "INSERT INTO Tags (pid, tag) VALUES ('$id', '$tag')";
+      if (!mysqli_query($conn, $sql)) {
+          echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+    }
 }
 ?>
 <html>
@@ -19,11 +23,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     if (isset($_SESSION['uid']) && ($_SESSION['uid'] == $row['uid'])) {
-        echo '<form action="post.php?id=' echo '$id' '"method="post" enctype="text/plain">
+        echo '<form action="post.php?id=' . $id . '" method="post" enctype="text/plain">
                 Add a tag to your image:
                 <input type="text" accept="text/plain" name="usertag" id="usertag">
                 <input type="submit" value="Add tag" name="submit">
-              </form>'
+              </form>';
     }
     ?>
 </body>
--- welcome.php
+++ welcome.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['username'])) {
 </head>
 <body>
     <h1>Welcome <?php echo '<a href="user.php?username=' . $_SESSION['username'] . '">' . $_SESSION['username'] . '</a>'; ?></h1>
-    <h2><a href="login.php">Sign Out</a></h2>
+    <h2><a href="logout.php">Sign Out</a></h2>
     <form action="upload.php" method="post" enctype="multipart/form-data">
         Select image to upload:
         <input type="file" accept="image/jpeg,image/gif,image/x-png" name="userimage" id="userimage">
