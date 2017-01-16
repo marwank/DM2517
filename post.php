eafab8bd5e25b12a75035131105257293edee246
Marwan Khalili <marwank@kth.se>
2017-01-16 15:24:47 +0100

    fixec bug

--- post.php
+++ post.php
@@ -1,20 +1,22 @@
 <?php
 include('config.php');
 session_start();
-$id = $_GET['id'];
-$sql = "SELECT * FROM Posts WHERE id = '$id'";
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($result)
+  if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM Posts WHERE id = '$id'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result)
+  }
 ?>
 <html>
 <body>
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
         if ($row) {
             echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
         }
-    }
-    
+
+
     if (isset($_SESSION['username'])) {
 
     }
