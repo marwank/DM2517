99aeb6eb72192c269a89e6b6e65e8907cd9f9d57
Isak Jagberg <ijagberg@gmail.com>
2017-01-16 15:28:38 +0100

    more post

--- post.php
+++ post.php
@@ -1,34 +1,28 @@
 <?php
 include('config.php');
 session_start();
-  if ($_SERVER["REQUEST_METHOD"] == "GET") {
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $id = $_GET['id'];
     $sql = "SELECT * FROM Posts WHERE id = '$id'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result)
-  }
+}
 ?>
 <html>
 <body>
     <?php
-
-        if ($row) {
-            echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
-        }
-
-
-    if (isset($_SESSION['username'])) {
-
+    if ($row) {
+        echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
     }
 
-    ?>
-    <form action="post.php" method="post" enctype="text/plain">
-        Add a tag to your image:
-        <input type="text" accept="text/plain" name="usertag" id="usertag">
-        <input type="submit" value="Add tag" name="submit">
-    </form>
 
-    <?php
+    if (isset($_SESSION['uid']) && ($_SESSION['uid'] == $row['uid'])) {
+        echo '<form action="post.php" method="post" enctype="text/plain">
+                Add a tag to your image:
+                <input type="text" accept="text/plain" name="usertag" id="usertag">
+                <input type="submit" value="Add tag" name="submit">
+              </form>'
+    }
 
     ?>
 </body>
