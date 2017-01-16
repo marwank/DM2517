d868343b9d98b3ae337a532104ae76cb36a56321
Marwan Khalili <marwank@kth.se>
2017-01-16 16:35:27 +0100

    Adding cmoments

--- post.php
+++ post.php
@@ -1,23 +1,31 @@
 <?php
 include('config.php');
 session_start();
-
-if ($tag = $_GET['tag']) {
-  $query = "INSERT INTO Tags (pid, tag) VALUES ('$id', '$tag')";
-  if (!mysqli_query($conn, $query)) {
-    echo "Error: " . $query . "<br>" . mysqli_error($conn);
-  }
-}
-if ($desc = $_GET['desc']) {
-  $query = "UPDATE Posts SET description = '$desc' WHERE id = '$id'";
-  if (!mysqli_query($conn, $query)) {
-    echo "Error: " . $query . "<br>" . mysqli_error($conn);
-  }
-}
 if ($id = $_GET['id']) {
+  if ($tag = $_POST['tag']) {
+    $query = "INSERT INTO Tags (pid, tag) VALUES ('$id', '$tag')";
+    if (!mysqli_query($conn, $query)) {
+      echo "Error: " . $query . "<br>" . mysqli_error($conn);
+    }
+  }
+
+  if ($comment = $_POST['comment'] && $uid = $_SESSION['uid']) {
+    $query = "INSERT INTO Comments (uid, pid, content) VALUES ('$uid', '$id', '$comment')";
+    if (!mysqli_query($conn, $query)) {
+      echo "Error: " . $query . "<br>" . mysqli_error($conn);
+    }
+  }
+
+  if ($desc = $_POST['desc']) {
+    $query = "UPDATE Posts SET description = '$desc' WHERE id = '$id'";
+    if (!mysqli_query($conn, $query)) {
+      echo "Error: " . $query . "<br>" . mysqli_error($conn);
+    }
+  }
+
   $sql = "SELECT * FROM Posts WHERE id = '$id'";
   $result = mysqli_query($conn, $sql);
-  $row = mysqli_fetch_assoc($result);  
+  $row = mysqli_fetch_assoc($result);
 }
 ?>
 <html>
@@ -29,26 +37,23 @@ if ($id = $_GET['id']) {
   }
 
   if (isset($_SESSION['uid']) && ($_SESSION['uid'] == $row['uid'])) {
-    echo '<form action="post.php" method="get">
+    echo '<form method="post" action="post.php?id=' . $id . '">
     Add a tag to your image:
     <input type="text" accept="text/plain" name="tag" id="tag"/>
-    <input type="hidden" name="id" value="' . $id . '"/>
     <input type="submit" value="Add tag"/>
     </form>';
-    echo '<form action="post.php" method="get">
+    echo '<form method="post" action="post.php?id=' . $id . '">
     Edit the description of your image:
     <input type="text" accept="text/plain" name="desc" id="desc">
-    <input type="hidden" name="id" value="' . $id . '"/>
     <input type="submit" value="Edit description">
     </form>';
   }
   if (isset($_SESSION['uid'])) {
-      echo '<form action="post.php" method="get">
-      Add a comment to this image:
-      <input type="text" accept="text/plain" name="comment" id="comment"/>
-      <input type="hidden" name="id" value="' . $id . '"/>
-      <input type="submit" value="Add comment" />
-      </form>';
+    echo '<form method="post" action="post.php?id=' . $id . '">
+    Add a comment to this image:
+    <input type="text" accept="text/plain" name="comment" id="comment"/>
+    <input type="submit" value="Add comment" />
+    </form>';
   }
   ?>
 </body>
