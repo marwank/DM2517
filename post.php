817368826f4d99def4d35e8835d7ea717bf03a2c
Marwan Khalili <marwank@kth.se>
2017-01-16 15:53:26 +0100

    Fixing bugs

--- post.php
+++ post.php
@@ -5,7 +5,9 @@ if ($id = $_GET['id']) {
     $sql = "SELECT * FROM Posts WHERE id = '$id'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
-    if ($tag = $_POST['usertag']) {
+    echo 'BEFORE TAG';
+    if ($tag = $_GET['usertag']) {
+      echo 'TAG WORKS';
       $query = "INSERT INTO Tags (pid, tag) VALUES ('$id', '$tag')";
       if (!mysqli_query($conn, $query)) {
           echo "Error: " . $query . "<br>" . mysqli_error($conn);
@@ -23,15 +25,10 @@ if ($id = $_GET['id']) {
     if (isset($_SESSION['uid']) && ($_SESSION['uid'] == $row['uid'])) {
         echo '<form action="post.php?id=' . $id . '" method="post" enctype="text/plain">
                 Add a tag to your image:
-                <input type="text" accept="text/plain" name="usertag" id="usertag">
-                <input type="submit" value="Add tag" name="submit">
+                <input type="text" accept="text/plain" name="usertag" id="usertag"/>
+                <input type="submit" value="Add tag" name="submit"/>
               </form>';
     }
     ?>
 </body>
 </html>
-
-
-
-
-<?php
--- welcome.php
+++ welcome.php
@@ -14,8 +14,8 @@ if (!isset($_SESSION['username'])) {
     <h2><a href="logout.php">Sign Out</a></h2>
     <form action="upload.php" method="post" enctype="multipart/form-data">
         Select image to upload:
-        <input type="file" accept="image/jpeg,image/gif,image/x-png" name="userimage" id="userimage">
-        <input type="submit" value="Upload Image" name="submit">
+        <input type="file" accept="image/jpeg,image/gif,image/x-png" name="userimage" id="userimage"/>
+        <input type="submit" value="Upload Image" name="submit"/>
     </form>
     <?php
     $sql = "SELECT image from Posts";
